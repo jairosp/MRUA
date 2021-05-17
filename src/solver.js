4b4bedfffc7ca88f81dea656da2b7f4834399661
Jairo Serrano <jairosp2000@gmail.com>
2021-05-17 21:27:44 +0200

    Debuggin magnitudes validas

--- src/solver.js
+++ src/solver.js
@@ -2,8 +2,10 @@ function problemSolver(velocidadA, velocidadB, distanciaAB){
     // debugger
     const time = distanciaAB / (velocidadA + velocidadB);
     const matchPoint = velocidadA * time;
-
-    return `Ambos coches se encuentran a ${Math.floor(matchPoint)} metros desde donde salió A, después de ${Math.floor(time)} segundos`
+    if (time && matchPoint){
+        return `Ambos coches se encuentran a ${Math.floor(matchPoint)} metros desde donde salió A, después de ${Math.floor(time)} segundos`
+    }
+    return 'Debe introducir todas las magnitudes'
 }
 
 export default problemSolver;
