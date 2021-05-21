3667401b7f3d812c30651424d29797606eaafc18
Jairo Serrano <jairosp2000@gmail.com>
2021-05-21 16:59:43 +0200

    Bugs fixed 21/05/2021

--- src/dinamicsolver.js
+++ src/dinamicsolver.js
@@ -6,12 +6,19 @@ function dinamicSolver(mass1, mass2, coeficient){
     const friction = coeficient * normal;
     const aceleration = (weight2 - friction) / (mass1 + mass2)
 
+    debugger
     const tension = mass1 * aceleration + friction;
 
-    if (aceleration && tension){
-        return `La aceleración del sistema es ${Math.round(aceleration * 100) / 100} m/s y fuerza de tensión es ${Math.round(tension * 100) / 100} N.`
+    if (aceleration && tension && aceleration > 0){
+        return `La aceleración del sistema es ${Math.round(aceleration * 100) / 100} m/s^2 y fuerza de tensión es ${Math.round(tension * 100) / 100} N.`;
     }
-    return 'Debe introducir todas las magnitudes'
+    else if(aceleration <= 0 && aceleration != null)
+    {
+        return `No hay movimiento y la tensión es ${Math.round(tension * 100) / 100} N.`;
+    }
+    else{
+        return 'Debe introducir todas las magnitudes';
+    }
 }
 
 export default dinamicSolver;
